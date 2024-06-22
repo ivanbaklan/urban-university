68182af555b87e2b6e370ab62146047eac09351a
ivan <ivanbaklan@gmail.com>
2024-06-22 18:30:29 +0400

    add module_2_8.py add print

--- module_2_8.py
+++ module_2_8.py
@@ -4,3 +4,7 @@ def get_multiplied_digits(number):
     if len(str_number) == 1:
         return first
     return first * get_multiplied_digits(int(str_number[1:]))
+
+
+result = get_multiplied_digits(40203)
+print(result)
