26c4e4e211c370e8b7caf7025c43223f4a2eeee2
ivan <ivanbaklan@gmail.com>
2024-06-22 18:33:10 +0400

    add module_2_8.py change if

--- module_2_8.py
+++ module_2_8.py
@@ -1,7 +1,7 @@
 def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
-    if len(str_number) == 1:
+    if not len(str_number) > 1:
         return first
     return first * get_multiplied_digits(int(str_number[1:]))
 
