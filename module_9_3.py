9747208ea322738835e53fafaa62d99bf121d5f9
ivan <ivanbaklan@gmail.com>
2024-07-24 15:15:20 +0400

    add module_9_3.py

---
+++ module_9_3.py
@@ -0,0 +1,12 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(item[0]) - len(item[1]) for item in zip(first, second) if len(item[0]) != len(item[1]))
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
+
+print(list(first_result))
+print(list(second_result))
+
+
+# [1, 2]
+# [False, False, True]
