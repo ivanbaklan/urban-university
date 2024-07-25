b04f2b7f6f5031405e83bac2799e8939ec625ae9
ivan <ivanbaklan@gmail.com>
2024-07-25 11:02:01 +0400

    add module_11_1.py

--- module_11_1.py
+++ module_11_1.py
@@ -3,6 +3,8 @@ Requests
 url: https://requests.readthedocs.io/en/latest/index.html
 Данная библиотека упрощает работ с запросами к HTTP/HTTPS ресурсам
 Пример использования:
+
+    GET method
     import requests
     r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
     >>> r.status_code
@@ -15,6 +17,35 @@ url: https://requests.readthedocs.io/en/latest/index.html
     '{"type":"User"...'
     >>> r.json()
     {'private_gists': 419, 'total_private_repos': 77, ...}
+
+    POST method
+    import requests
+    payload = {'key1': 'value1', 'key2': 'value2'}
+    >>> r = requests.post('https://httpbin.org/post', data=payload)
+    >>> print(r.text)
+    {
+      ...
+      "form": {
+        "key2": "value2",
+        "key1": "value1"
+      },
+      ...
+    }
+
+    POST a Multipart-Encoded File
+    import requests
+    url = 'https://httpbin.org/post'
+    >>> files = {'file': open('report.xls', 'rb')}
+
+    >>> r = requests.post(url, files=files)
+    >>> r.text
+    {
+      ...
+      "files": {
+        "file": "<censored...binary...data>"
+      },
+      ...
+    }
 """
 
 """
@@ -22,9 +53,22 @@ NumPy
 url: https://numpy.org/doc/stable/user/absolute_beginners.html
 Данная библиотека упрощает и ускоряет работу с массивами чисел и операциями над ними.
 Пример использования:
+    
+    Create array
+    import numpy as np
+    >>> a = np.array([1, 2, 3, 4, 5, 6])
+    >>> a
+    array([1, 2, 3, 4, 5, 6])
+
+    Применение метода shape к массиву
     import numpy as np
     >>> a = np.array([[1, 2, 3],
     ...               [4, 5, 6]])
     >>> a.shape
     (2, 3)
+    
+    Пример сортировки массива
+    >>> arr = np.array([2, 1, 5, 3, 7, 4, 6, 8])
+    >>> np.sort(arr)
+    array([1, 2, 3, 4, 5, 6, 7, 8])
 """
