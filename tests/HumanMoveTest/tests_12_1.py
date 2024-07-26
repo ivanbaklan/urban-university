69b6de8fedf4f1900818fb3115254245a12b3644
ivan <ivanbaklan@gmail.com>
2024-07-26 13:03:42 +0400

    add test_12_1

---
+++ tests/HumanMoveTest/tests_12_1.py
@@ -0,0 +1,30 @@
+import unittest
+
+import runner
+
+
+class RunnerTes(unittest.TestCase):
+
+    def test_walk(self):
+        test_runner = runner.Runner("test_runner")
+        for _ in range(10):
+            test_runner.walk()
+        self.assertEqual(test_runner.distance, 50)
+
+    def test_run(self):
+        test_runner = runner.Runner("test_runner")
+        for _ in range(10):
+            test_runner.run()
+        self.assertEqual(test_runner.distance, 100)
+
+    def test_challenge(self):
+        test_runner_1 = runner.Runner("test_runner_1")
+        test_runner_2 = runner.Runner("test_runner_2")
+        for _ in range(10):
+            test_runner_1.walk()
+            test_runner_2.run()
+        self.assertNotEqual(test_runner_1.distance, test_runner_2.distance)
+
+
+if __name__ == "__main__":
+    unittest.main()
