f8f37100029cbcc7406c30eb448f7744ab0a5e14
ivan <ivanbaklan@gmail.com>
2024-07-26 15:33:35 +0400

    add test_12_2.py

--- tests/HumanMoveTest/tests_12_2.py
+++ tests/HumanMoveTest/tests_12_2.py
@@ -59,26 +59,6 @@ class TournamentTest(unittest.TestCase):
         print(self.runner_2.name)
         self.assertEqual(results[2].name, self.runner_2.name)
 
-    def test_walk(self):
-        test_runner = Runner("test_runner")
-        for _ in range(10):
-            test_runner.walk()
-        self.assertEqual(test_runner.distance, 50)
-
-    def test_run(self):
-        test_runner = Runner("test_runner")
-        for _ in range(10):
-            test_runner.run()
-        self.assertEqual(test_runner.distance, 100)
-
-    def test_challenge(self):
-        test_runner_1 = Runner("test_runner_1")
-        test_runner_2 = Runner("test_runner_2")
-        for _ in range(10):
-            test_runner_1.walk()
-            test_runner_2.run()
-        self.assertNotEqual(test_runner_1.distance, test_runner_2.distance)
-
 
 if __name__ == "__main__":
     unittest.main()
