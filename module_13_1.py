303137b8db50180e058a1f02383f6bc8c4ea3783
ivan <ivanbaklan@gmail.com>
2024-07-30 14:06:17 +0400

    add tests_13_1.py

--- module_13_1.py
+++ module_13_1.py
@@ -2,20 +2,21 @@ import asyncio
 
 
 async def start_strongman(name, power):
-    print(f'Силач {name} начал соревнования.')
+    print(f"Силач {name} начал соревнования.")
     for i in range(1, 6):
-        await asyncio.sleep(1/power)
-        print(f'Силач {name} поднял {i} шар')
+        await asyncio.sleep(1 / power)
+        print(f"Силач {name} поднял {i} шар")
     print(f"Силач {name} закончил соревнования.")
 
 
 async def start_tournament():
-    task_1 = asyncio.create_task(start_strongman('Pasha', 3))
-    task_2 = asyncio.create_task(start_strongman('Denis', 4))
-    task_3 = asyncio.create_task(start_strongman('Apollon', 5))
+    task_1 = asyncio.create_task(start_strongman("Pasha", 3))
+    task_2 = asyncio.create_task(start_strongman("Denis", 4))
+    task_3 = asyncio.create_task(start_strongman("Apollon", 5))
     await task_1
     await task_2
     await task_3
+
 
 if __name__ == "__main__":
     asyncio.run(start_tournament())
