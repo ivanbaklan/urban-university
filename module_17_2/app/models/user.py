7575984fc496561bc3b4cd3367d3e0bf1b428deb
ivan <ivanbaklan@gmail.com>
2024-11-10 06:33:35 +0300

    fix model user 17_1, 17_2, 17_3

--- module_17_2/app/models/user.py
+++ module_17_2/app/models/user.py
@@ -12,4 +12,4 @@ class User(Base):
     lastname = Column(String)
     age = Column(Integer)
     slug = Column(String, unique=True, index=True)
-    user = relationship("Task", back_populates="user")
+    tasks = relationship("Task", back_populates="user")
